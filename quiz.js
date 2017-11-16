8d160c06e3ed9baf872f6ee3e76f3af671315eeb
meigh28 <meghshreekher3@gmail.com>
2017-11-16 14:12:40 -0800

    Initial commit

---
+++ quiz.js
@@ -0,0 +1,50 @@
+var questions = [
+{
+	"question":"In a class of 78 students 41 are taking French, 22 are taking German. Of the students taking French or German, 9 are taking both courses. How many students are not enrolled in either course?",
+	"option1" : "6",
+	"option2"  : "15",
+	"option3" : "34",
+	"option4"  : "23",
+	"answer"  : "2"
+
+},
+{
+	"question":"If an object travels at five feet per second, how many feet does it travel in one hour?",
+	"option1" : "30",
+	"option2"  : "300",
+	"option3" : "720",
+	"option4"  : "1800",
+	"answer"  : "4"
+
+},
+{
+	"question":" What is the average (arithmetic mean) of all the multiples of ten from 10 to 190 inclusive??",
+	"option1" : "90",
+	"option2"  : "95",
+	"option3" : "100",
+	"option4"  : "105",
+	"answer"  : "3"
+
+},
+{
+	"question":" A cubical block of metal weighs 6 pounds. How much will another cube of the same metal weigh if its sides are twice as long??",
+	"option1" : "48",
+	"option2"  : "32",
+	"option3" : "28",
+	"option4"  : "18",
+	"answer"  : "2"
+
+},
+
+{
+	"question" : "what is ( √2 - √3 )² = ",
+	"option1" : "5 - 2√6 ",
+	"option2"  : "5 - √6 ",
+	"option3" : "1 - 2√6 ",
+	"option4"  : "1 - √2 ",
+	"answer"  : "2"
+
+}];
+
+
+
